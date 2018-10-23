272214ebe0cd549737d2552b6b02de980dbb3b56
Juan Gonzalez <jgonzalez88@live.com>
2018-10-23 14:33:27 -0500

    Add static assets

---
+++ GapInsurance.Web/Apps/gap-insurance/build/precache-manifest.0ef947da0d9368911764cca1a7f0bc98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "106ff840011025841442",
+    "url": "/static/js/main.106ff840.chunk.js"
+  },
+  {
+    "revision": "4d8a9795caf76083e808",
+    "url": "/static/js/1.4d8a9795.chunk.js"
+  },
+  {
+    "revision": "106ff840011025841442",
+    "url": "/static/css/main.15d01969.chunk.css"
+  },
+  {
+    "revision": "4d8a9795caf76083e808",
+    "url": "/static/css/1.e7389225.chunk.css"
+  },
+  {
+    "revision": "56140f94e06268f39eb5878abfd69aa2",
+    "url": "/index.html"
+  }
+];
